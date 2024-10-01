695cf0edf695a7fa3a8089944dfe9f2d62ae4c13
Israel <israel_alves77@hotmail.com>
2024-10-01 17:11:47 -0300

    feat: Modelagem inicial ORM da composição de entidades Bank, CurrentAccount e SavingAccount

---
+++ src/main/java/tec/challenge/bank/models/Bank.java
@@ -0,0 +1,34 @@
+package tec.challenge.bank.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_banks")
+public class Bank {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToMany(mappedBy = "bank", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<CurrentAccount> currentAccounts = new ArrayList<>();
+
+  @OneToMany(mappedBy = "bank", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<SavingAccount> savingsAccounts = new ArrayList<>();
+}
